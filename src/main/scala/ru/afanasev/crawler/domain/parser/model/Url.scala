e8c8a3f8172e1df85b6a330f1388ababa86e2aaf
Evgeny Afanasev <eafanasev@intech-global.com>
2022-03-11 10:49:59 +0300

    type refactoring

--- src/main/scala/ru/afanasev/crawler/domain/parser/model/Url.scala
+++ src/main/scala/ru/afanasev/crawler/domain/parser/model/Url.scala
@@ -2,6 +2,7 @@ package ru.afanasev.crawler.domain.parser.model
 
 import cats.implicits._
 import io.estatico.newtype.macros.newtype
+import ru.afanasev.crawler.domain.parser.model.Result.UrlAndResult
 
 
 
@@ -11,7 +12,7 @@ object Url {
 
   private val urlPattern = "https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)".r
 
-  def mkUrl(url: String): Either[(String, String), Valid] =
+  def mkUrl(url: String): Either[UrlAndResult, Valid] =
     if(urlPattern.matches(url)) Valid(url).asRight
     else (url -> "url is incorrect").asLeft
 }
